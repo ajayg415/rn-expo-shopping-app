181aeacdad9aa0ae2c6484cf3560a0800241475a
ajayg415 <ajay.g415@gmail.com>
2021-07-10 11:25:21 +0530

    added packages and empty screens

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ProductsOverviewScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>ProductsOverviewScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {},
+});
+
+export default ProductsOverviewScreen;
